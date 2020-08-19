b9588246226a0ecf66b9a6fc55a707c1b8620e6b
Konrad Rokicki <rokickik@janelia.hhmi.org>
2020-08-18 21:17:10 -0400

    clean up

--- cmd/push.go
+++ cmd/push.go
@@ -1,27 +1,21 @@
 package cmd
 
 import (
-	//"context"
-	//"encoding/base64"
-	//"encoding/json"
-	//"github.com/docker/docker/pkg/jsonmessage"
-	//"github.com/docker/docker/api/types"
-	//"github.com/docker/docker/client"
-	//"github.com/moby/term"
 	"github.com/spf13/cobra"
 	Utils "maru/utils"
 )
 
 var pushCmd = &cobra.Command{
 	Use:   "push",
-	Short: "Push the built container to all its registered namespaces",
-	Long: ``,
+	Short: "Push the container to all its configured namespaces",
+	Long: `Deploys the container to all of its configured namespaces. The container must be already built using the build command. Use namespace command to list namespaces or add a new one.`,
+	Args: cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 
-		config := Utils.ReadProjectConfig()
+		config := Utils.ReadMandatoryProjectConfig()
 		if !config.HasNamespaces() {
 			Utils.PrintMessage("There are no namespaces configured for the current project.")
-			Utils.PrintInfo("Use `maru namespace add` to add a new namespace, and `maru namespace rm` to delete one.")
+			Utils.PrintInfo("Use `maru namespace add` to add a new namespace.")
 		} else {
 			imageName := config.GetNameVersion()
 			Utils.PrintInfo("Pushing %s to %d repositories", imageName, len(config.Namespaces))
@@ -42,46 +36,6 @@ var pushCmd = &cobra.Command{
 						Utils.PrintSuccess("Successfully pushed to %s", registryTag)
 					}
 				}
-
-				/*
-				cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-				if err != nil {
-					Utils.PrintFatal("%s", err)
-				}
-				defer cli.Close()
-
-				ctx := context.Background()
-
-				// Tag the image to include the registry and namespace
-				err = cli.ImageTag(ctx, imageName, registryTag)
-				if err != nil {
-					Utils.PrintFatal("%s", err)
-				}
-
-				authConfig := types.AuthConfig{
-					Username: username,
-					Password: password,
-				}
-				encodedJSON, err := json.Marshal(authConfig)
-				if err != nil {
-					panic(err)
-				}
-				authStr := base64.URLEncoding.EncodeToString(encodedJSON)
-
-				reader, err := cli.ImagePush(ctx, registryTag, types.ImagePushOptions{
-					RegistryAuth: authStr,
-				})
-				if err != nil {
-					Utils.PrintFatal("%s", err)
-				}
-
-				termFd, isTerm := term.GetFdInfo(os.Stderr)
-				defer reader.Close()
-				err2 := jsonmessage.DisplayJSONMessagesStream(reader, os.Stderr, termFd, isTerm, nil)
-				if err2 != nil {
-					Utils.PrintFatal("%s", err2)
-				}
-				*/
 			}
 
 		}
